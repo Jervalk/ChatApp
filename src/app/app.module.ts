2c50dd153e8e4f3cfd23675a117458de9055a30b
Jeremy Primat <valk.jeremy@gmail.com>
2021-03-25 11:05:28 +0100

    Added new components : messages + conversation + chat-form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContactsComponent } from './contacts/contacts.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ChatFormComponent } from './chat-form/chat-form.component';
+import { MessagesComponent } from './messages/messages.component';
+import { ConversationComponent } from './conversation/conversation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactsComponent,
+    ChatFormComponent,
+    MessagesComponent,
+    ConversationComponent,
   ],
   imports: [
     BrowserModule,
